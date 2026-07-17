fadeec0101de16d66c21bb15fe8c05e72de233c7
Eddie Altamirano <eddaaltamiranopl@ittepic.edu.mx>
2026-07-17 13:15:01 -0700

    Initial commit

---
+++ FORMULARIOS/Forma_LCD.pas
@@ -0,0 +1,102 @@
+unit Forma_LCD;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.pngimage, Vcl.Imaging.jpeg,
+  Vcl.ExtCtrls, Vcl.StdCtrls, ShellAPI, MMSystem;
+
+type
+  TFormaLCD = class(TForm)
+    Panel1: TPanel;
+    Image1: TImage;
+    Image2: TImage;
+    Panel2: TPanel;
+    Button4: TButton;
+    Label2: TLabel;
+    Label1: TLabel;
+    Image3: TImage;
+    laHora: TLabel;
+    Button1: TButton;
+    memoAsm: TMemo;
+    editPrograma: TEdit;
+    editTexto: TEdit;
+    procedure Button4Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    emu86,texto, enter,APOSTROFE,INICIO,FIN, programaS:String;
+    labelPrograma: TLabel;
+    Archivo: TextFile;
+    prog, mensaje:String;
+  end;
+
+var
+  FormaLCD: TFormaLCD;
+
+implementation
+
+{$R *.dfm}
+
+procedure TFormaLCD.Button1Click(Sender: TObject);
+begin
+  try
+      prog:='C:\Users\ealta\Downloads'+editPrograma.Text+'.asm';
+      texto:=editTexto.Text;
+        IF(Length(prog)<>0) AND (Length(texto)<>0) THEN
+        BEGIN
+            emu86:='.MODEL SMALL'+ENTER+
+            '.STACK'+ENTER+
+            '.DATA'+ENTER+
+            'CADENA DB '+APOSTROFE+
+            editTexto.Text+APOSTROFE+ENTER+
+            '.CODE'+ENTER+ENTER+
+            INICIO+ENTER+ENTER+
+            'MOV DX,2040H'+ENTER+
+            'MOV SI,0'+ENTER+
+            'MOV AL,CADENA[SI]'+ENTER+
+            'OUT DX,AL'+ENTER+FIN;
+
+            // creación del archivo
+            AssignFile(Archivo,prog);
+            Rewrite(Archivo);
+            Write(Archivo,emu86);
+            laHora.Caption := 'Hora de Envío: '+TimeToStr(now);
+            sndPlaySound('C:\PROYECTO HIBRIDO MULTIPLATAFORMA\SONIDOS\new-notification-022-370046.wav',SND_FILENAME or SND_ASYNC);
+            closeFile(Archivo);
+            MemoAsm.Lines.LoadFromFile(prog);
+            //
+            ShellExecute(FormaLCD.handle,nil,pChar(prog),nil,nil,SW_SHOWNORMAL);
+
+        END
+        ELSE
+          BEGIN
+              ShowMessage('Favor de proporcionar datos.');
+          END;
+  except
+      showMessage('Error al generar el archivo LCD .asm');
+  end // Fin TRY
+end;
+
+procedure TFormaLCD.Button4Click(Sender: TObject);
+begin
+  close;
+end;
+
+procedure TFormaLCD.FormActivate(Sender: TObject);
+begin
+  enter:=#13#10;
+  APOSTROFE:='''';
+  INICIO:= 'MOV AX,@DATA'+ENTER+
+          'MOV DS, AX'+ENTER;
+
+  INICIO:= 'MOV AX,4C00H'+ENTER+
+          'INT 21H'+ENTER+
+          'END';
+
+end;
+
+end.
